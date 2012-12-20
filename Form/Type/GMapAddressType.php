cbb7dcd5250e607efa8701d5bd130b6d15b75f1d
Malian <deronmalian@gmail.com>
2012-12-20 12:25:58 +0100

    Remove deprecated fonction in Symfony 2.3

--- Form/Type/GMapAddressType.php
+++ Form/Type/GMapAddressType.php
@@ -15,6 +15,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
 
 /**
  * A GMap address type.
@@ -52,7 +54,7 @@ class GMapAddressType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getDefaultOptions(array $options)
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         return array(
             'virtual'   => true,
