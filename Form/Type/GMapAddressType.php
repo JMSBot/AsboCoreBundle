0234e529c8a5eaa420660ea9680fb5c4154c2907
Malian <deronmalian@gmail.com>
2012-12-20 12:36:28 +0100

    Revert "Remove deprecated fonction in Symfony 2.3"
    This reverts commit cbb7dcd5250e607efa8701d5bd130b6d15b75f1d.

--- Form/Type/GMapAddressType.php
+++ Form/Type/GMapAddressType.php
@@ -15,8 +15,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\OptionsResolver\Options;
 
 /**
  * A GMap address type.
@@ -54,7 +52,7 @@ class GMapAddressType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function getDefaultOptions(array $options)
     {
         return array(
             'virtual'   => true,
